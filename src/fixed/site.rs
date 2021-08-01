44bfbe388d1c2b71088730a9d48e90d6c295b7a0
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-02 00:56:50 +0200

    fix: Just write every Facility Kind manually

--- src/fixed/site.rs
+++ src/fixed/site.rs
@@ -1,12 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-use super::facility;
-
 #[derive(Debug, Serialize, Deserialize)]
 #[cfg_attr(test, derive(ts_rs::TS))]
 #[serde(rename_all = "camelCase", rename = "SiteKind")]
 pub enum Kind {
-    Facility(facility::Identifier),
+    FacilityStargate,
+    FacilityStation,
     AsteroidField,
 }
 
