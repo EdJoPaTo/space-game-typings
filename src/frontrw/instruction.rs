ec2ae20f49d975f8a1eb666fa7e6e22f7cfc8a7d
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-05 10:27:07 +0200

    feat(instruction): order and computation step

--- src/frontrw/instruction.rs
+++ src/frontrw/instruction.rs
@@ -2,71 +2,91 @@ use serde::{Deserialize, Serialize};
 
 use crate::fixed::facility::Service;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 // #[cfg_attr(test, derive(ts_rs::TS))]
-#[serde(rename_all = "camelCase", tag = "type")]
+#[serde(rename_all = "camelCase", tag = "step")]
 pub enum Instruction {
-    Undock(Undock),
-    Warp(Warp),
-    UseFacility(UseFacility),
-    ModuleTargeted(ModuleTargeted),
-    ModuleUntargeted(ModuleUntargeted),
+    Untargeted(Untargeted),
+    Targeted(Targeted),
+    Movement(Movement),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+// #[cfg_attr(test, derive(ts_rs::TS))]
+#[serde(rename_all = "camelCase", tag = "type")]
+pub enum Untargeted {
+    Module(ModuleUntargeted),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+// #[cfg_attr(test, derive(ts_rs::TS))]
+#[serde(rename_all = "camelCase", tag = "type")]
+pub enum Targeted {
+    Facility(Facility),
+    Module(ModuleTargeted),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+// #[cfg_attr(test, derive(ts_rs::TS))]
+#[serde(rename_all = "camelCase", tag = "type")]
+pub enum Movement {
+    Undock(Undock),
+    Warp(Warp),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(test, derive(ts_rs::TS))]
-#[serde(rename_all = "camelCase", rename = "InstructionModuleUntargeted")]
+#[serde(rename_all = "camelCase", rename = "InstructionUntargetedModule")]
 pub struct ModuleUntargeted {
     pub module_index: u8,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(test, derive(ts_rs::TS))]
-#[serde(rename_all = "camelCase", rename = "InstructionModuleTargeted")]
+#[serde(rename_all = "camelCase", rename = "InstructionTargetedModule")]
 pub struct ModuleTargeted {
     pub target_index_in_site: u8,
     pub module_index: u8,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(test, derive(ts_rs::TS))]
-#[serde(rename_all = "camelCase", rename = "InstructionUseFacility")]
-pub struct UseFacility {
+#[serde(rename_all = "camelCase", rename = "InstructionTargetedFacility")]
+pub struct Facility {
     pub target_index_in_site: u8,
     pub service: Service,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(test, derive(ts_rs::TS))]
-#[serde(rename_all = "camelCase", rename = "InstructionWarp")]
+#[serde(rename_all = "camelCase", rename = "InstructionMovementWarp")]
 pub struct Warp {
     pub site_unique: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(test, derive(ts_rs::TS))]
-#[serde(rename_all = "camelCase", rename = "InstructionUndock")]
+#[serde(rename_all = "camelCase", rename = "InstructionMovementUndock")]
 pub struct Undock {
     ship_id: u8,
 }
 
 #[cfg(test)]
 ts_rs::export! {
-    // Instruction => "instruction.ts",
-    ModuleUntargeted => "instruction-module-untargeted.ts",
-    ModuleTargeted => "instruction-module-targeted.ts",
-    UseFacility => "instruction-use-facility.ts",
-    Warp => "instruction-warp.ts",
-    Undock => "instruction-undock.ts",
+    ModuleUntargeted => "instruction-untargeted-module.ts",
+    ModuleTargeted => "instruction-targeted-module.ts",
+    Facility => "instruction-targeted-facility.ts",
+    Warp => "instruction-movement-warp.ts",
+    Undock => "instruction-movement-undock.ts",
 }
 
 #[test]
 fn can_identify_undock() -> anyhow::Result<()> {
-    let data = Instruction::Undock(Undock { ship_id: 42 });
+    let data = Instruction::Movement(Movement::Undock(Undock { ship_id: 42 }));
     let json = serde_json::to_string_pretty(&data)?;
     println!("json {}", json);
     let some = serde_json::from_str::<Instruction>(&json)?;
-    if let Instruction::Undock(_) = some {
+    if let Instruction::Movement(Movement::Undock(_)) = some {
         Ok(())
     } else {
         panic!("wrong!");
@@ -75,13 +95,13 @@ fn can_identify_undock() -> anyhow::Result<()> {
 
 #[test]
 fn can_identify_warp() -> anyhow::Result<()> {
-    let data = Instruction::Warp(Warp {
+    let data = Instruction::Movement(Movement::Warp(Warp {
         site_unique: "666".to_string(),
-    });
+    }));
     let json = serde_json::to_string_pretty(&data)?;
     println!("json {}", json);
     let some = serde_json::from_str::<Instruction>(&json)?;
-    if let Instruction::Warp(_) = some {
+    if let Instruction::Movement(Movement::Warp(_)) = some {
         Ok(())
     } else {
         panic!("wrong!");
@@ -90,14 +110,14 @@ fn can_identify_warp() -> anyhow::Result<()> {
 
 #[test]
 fn can_identify_facility() -> anyhow::Result<()> {
-    let data = Instruction::UseFacility(UseFacility {
+    let data = Instruction::Targeted(Targeted::Facility(Facility {
         target_index_in_site: 42,
         service: Service::Dock,
-    });
+    }));
     let json = serde_json::to_string_pretty(&data)?;
     println!("json {}", json);
     let some = serde_json::from_str::<Instruction>(&json)?;
-    if let Instruction::UseFacility(_) = some {
+    if let Instruction::Targeted(Targeted::Facility(_)) = some {
         Ok(())
     } else {
         panic!("wrong!");
@@ -106,11 +126,11 @@ fn can_identify_facility() -> anyhow::Result<()> {
 
 #[test]
 fn can_identify_module_untargeted() -> anyhow::Result<()> {
-    let data = Instruction::ModuleUntargeted(ModuleUntargeted { module_index: 4 });
+    let data = Instruction::Untargeted(Untargeted::Module(ModuleUntargeted { module_index: 4 }));
     let json = serde_json::to_string_pretty(&data)?;
     println!("json {}", json);
     let some = serde_json::from_str::<Instruction>(&json)?;
-    if let Instruction::ModuleUntargeted(_) = some {
+    if let Instruction::Untargeted(Untargeted::Module(_)) = some {
         Ok(())
     } else {
         panic!("wrong!");
@@ -119,14 +139,14 @@ fn can_identify_module_untargeted() -> anyhow::Result<()> {
 
 #[test]
 fn can_identify_module_targeted() -> anyhow::Result<()> {
-    let data = Instruction::ModuleTargeted(ModuleTargeted {
+    let data = Instruction::Targeted(Targeted::Module(ModuleTargeted {
         target_index_in_site: 42,
         module_index: 4,
-    });
+    }));
     let json = serde_json::to_string_pretty(&data)?;
     println!("json {}", json);
     let some = serde_json::from_str::<Instruction>(&json)?;
-    if let Instruction::ModuleTargeted(_) = some {
+    if let Instruction::Targeted(Targeted::Module(_)) = some {
         Ok(())
     } else {
         panic!("wrong!");
