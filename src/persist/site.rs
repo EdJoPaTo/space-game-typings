738792fdafff1a90903e37fae499d6c86b5cb3d1
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-05 15:19:20 +0200

    feat(SiteInfo): generate statics simpler

--- src/persist/site.rs
+++ src/persist/site.rs
@@ -3,6 +3,7 @@ use std::collections::BTreeMap;
 use serde::{Deserialize, Serialize};
 
 use crate::fixed::site::Kind;
+use crate::fixed::solarsystem;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 #[cfg_attr(test, derive(ts_rs::TS))]
@@ -19,6 +20,32 @@ pub type SitesNearPlanet = BTreeMap<u8, Vec<Info>>;
 #[cfg(test)]
 ts_rs::export! {
     Info => "site-info.ts",
+}
+
+impl Info {
+    #[must_use]
+    pub fn generate_station(solarsystem: &solarsystem::Identifier, station_index: u8) -> Self {
+        let number = station_index + 1;
+        // TODO: römisch
+        let name = format!("{} {}", solarsystem, number);
+        let unique = format!("station{}", number);
+        Self {
+            kind: Kind::Station,
+            name: Some(name),
+            unique,
+        }
+    }
+
+    #[must_use]
+    pub fn generate_stargate(target_solarsystem: &solarsystem::Identifier) -> Self {
+        let name = target_solarsystem.to_string();
+        let unique = format!("stargate{}", target_solarsystem);
+        Self {
+            kind: Kind::Stargate,
+            name: Some(name),
+            unique,
+        }
+    }
 }
 
 #[test]
