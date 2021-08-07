c12db7276754d1c405469e5726897f5bb5a90dc1
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-07 11:59:23 +0200

    feat(ship): more useful Status methods

--- src/persist/ship.rs
+++ src/persist/ship.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::fixed::{module, shiplayout, ShipLayouts, Statics};
+use crate::fixed::{module, shiplayout, Statics};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 #[cfg_attr(test, derive(ts_rs::TS))]
@@ -73,13 +73,45 @@ impl Fitting {
 
 impl Status {
     #[must_use]
-    pub fn new(layouts: &ShipLayouts, fitting: &Fitting) -> Option<Self> {
-        let layout = layouts.get(&fitting.layout)?;
-        Some(Self {
+    pub fn new(statics: &Statics, fitting: &Fitting) -> Option<Self> {
+        let layout = statics.ship_layouts.get(&fitting.layout)?;
+        let mut status = Status {
             capacitor: layout.capacitor,
             hitpoints_armor: layout.hitpoints_armor,
             hitpoints_structure: layout.hitpoints_structure,
-        })
+        };
+        for passive_identifier in &fitting.slots_passive {
+            if let Some(module) = statics.modules_passive.get(passive_identifier) {
+                if let Some(armor) = module.hitpoints_armor {
+                    status.hitpoints_armor = status.hitpoints_armor.saturating_add(armor);
+                }
+                if let Some(capacitor) = module.capacitor {
+                    status.capacitor = status.capacitor.saturating_add(capacitor);
+                }
+            }
+        }
+        Some(status)
+    }
+
+    #[must_use]
+    /// Returns the minimum of two status thingies.
+    /// Helpful when ensuring a status is still within the ships limits
+    /// # Example
+    /// ```
+    /// use typings::persist::ship::Status;
+    /// let status = Status {capacitor: 20, hitpoints_armor: 42, hitpoints_structure: 4};
+    /// let ship_maximum = Status {capacitor: 20, hitpoints_armor: 20, hitpoints_structure: 10};
+    /// let min = status.min(&ship_maximum);
+    /// assert_eq!(min.capacitor, 20);
+    /// assert_eq!(min.hitpoints_armor, 20);
+    /// assert_eq!(min.hitpoints_structure, 4);
+    /// ```
+    pub fn min(&self, other: &Self) -> Self {
+        Self {
+            capacitor: self.capacitor.min(other.capacitor),
+            hitpoints_armor: self.hitpoints_armor.min(other.hitpoints_armor),
+            hitpoints_structure: self.hitpoints_structure.min(other.hitpoints_structure),
+        }
     }
 }
 
@@ -87,4 +119,46 @@ impl Status {
 fn default_fitting_is_valid() {
     let statics = crate::fixed::Statics::default();
     assert!(Fitting::default().is_valid(&statics));
+}
+
+#[test]
+fn status_without_modules_correct() {
+    let statics = Statics::default();
+    let expected = statics.ship_layouts.get("shiplayoutFrigate").unwrap();
+    let fitting = Fitting {
+        layout: "shiplayoutFrigate".to_string(),
+        slots_targeted: vec![],
+        slots_untargeted: vec![],
+        slots_passive: vec![],
+    };
+    let result = Status::new(&statics, &fitting);
+    assert_eq!(
+        result,
+        Some(Status {
+            capacitor: expected.capacitor,
+            hitpoints_armor: expected.hitpoints_armor,
+            hitpoints_structure: expected.hitpoints_structure,
+        })
+    );
+}
+
+#[test]
+fn status_of_default_fitting_correct() {
+    let statics = Statics::default();
+    let fitting = Fitting::default();
+    let expected_layout = statics.ship_layouts.get(&fitting.layout).unwrap();
+    let expected_passive = statics
+        .modules_passive
+        .get(&fitting.slots_passive[0])
+        .unwrap();
+    let result = Status::new(&statics, &fitting);
+    assert_eq!(
+        result,
+        Some(Status {
+            capacitor: expected_layout.capacitor,
+            hitpoints_armor: expected_layout.hitpoints_armor
+                + expected_passive.hitpoints_armor.unwrap(),
+            hitpoints_structure: expected_layout.hitpoints_structure,
+        })
+    );
 }
