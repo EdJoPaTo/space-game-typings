d919d06b87d9854b29c0a7a047d1dd84ef8a9279
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-22 21:43:44 +0200

    feat: station stores ships with collateral and so on

--- src/persist/player_assets.rs
+++ src/persist/player_assets.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-use crate::ship::Fitting;
+use crate::ship::Ship;
 
 #[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq)]
 #[cfg_attr(feature = "typescript", derive(ts_rs::TS))]
 #[serde(rename_all = "camelCase")]
 pub struct PlayerStationAssets {
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub ships: Vec<Fitting>,
+    pub ships: Vec<Ship>,
 }
 
 #[cfg(feature = "typescript")]
