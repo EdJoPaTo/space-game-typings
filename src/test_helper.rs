dcaaa4665921c1780bae19ced5abf78d490c8a52
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-27 13:01:18 +0200

    test(serde): check both json and yaml

--- src/test_helper.rs
+++ src/test_helper.rs
@@ -2,26 +2,32 @@ pub fn can_serde_parse<T>(input: &T)
 where
     T: std::fmt::Debug + std::cmp::PartialEq + serde::Serialize + serde::de::DeserializeOwned,
 {
-    println!("input {:?}", input);
+    dbg!(input);
+
     let json = serde_json::to_string_pretty(&input).unwrap();
     println!("json {}", json);
-    let parsed = serde_json::from_str::<T>(&json).unwrap();
-    println!("parsed {:?}", parsed);
+    let yaml = serde_yaml::to_string(&input).unwrap();
+    println!("yaml\n{}", yaml);
 
-    assert_eq!(input, &parsed);
+    let json_parsed = serde_json::from_str::<T>(&json).unwrap();
+    dbg!(&json_parsed);
+    let yaml_parsed = serde_yaml::from_str::<T>(&yaml).unwrap();
+    dbg!(&yaml_parsed);
+
+    assert_eq!(input, &json_parsed);
+    assert_eq!(input, &yaml_parsed);
 }
 
 pub fn can_string_parse<T>(input: &T)
 where
     T: std::fmt::Debug + std::cmp::PartialEq + ToString + std::str::FromStr,
 {
-    println!("input {:?}", input);
+    dbg!(input);
     let json = input.to_string();
     println!("string {}", json);
     let parsed = json
         .parse::<T>()
         .unwrap_or_else(|_| panic!("failed to parse"));
-    println!("parsed {:?}", parsed);
-
+    dbg!(&parsed);
     assert_eq!(input, &parsed);
 }
