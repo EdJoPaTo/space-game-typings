abd1e1d84b3b1a4804fc70e3a70dc4e97b38ac75
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-31 19:15:28 +0200

    build: remove cargo features
    its not worth the complexity

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,12 @@ pub mod entity;
 /// Static data which is read-only for everyone.
 /// It only changes on game-updates.
 pub mod fixed;
+pub mod market;
 pub mod player;
 pub mod ship;
 pub mod site;
 pub mod station;
 pub mod storage;
-
-#[cfg(feature = "market")]
-pub mod market;
 
 #[cfg(test)]
 mod test_helper;
--- src/site/mod.rs
+++ src/site/mod.rs
@@ -2,16 +2,13 @@ mod entity;
 pub mod entity_frontread;
 pub mod instruction;
 mod log;
+mod round;
 mod sites;
 
 pub use entity::{Entity, EntityAsteroid};
 pub use log::{Actor, Log};
-pub use sites::{Site, SitesNearPlanet};
-
-#[cfg(feature = "site_round")]
-mod round;
-#[cfg(feature = "site_round")]
 pub use round::{advance, Output};
+pub use sites::{Site, SitesNearPlanet};
 
 #[cfg(feature = "typescript")]
 ts_rs::export! {
