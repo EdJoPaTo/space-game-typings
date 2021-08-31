4c6b452d09d55de2040cf5532034e7ff3b1c1185
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-31 18:55:27 +0200

    feat(player): current ship in station assets

--- src/player/station_assets.rs
+++ src/player/station_assets.rs
@@ -8,6 +8,18 @@ use crate::storage::Storage;
 #[cfg_attr(feature = "typescript", derive(ts_rs::TS))]
 #[serde(rename_all = "camelCase", rename = "PlayerStationAssets")]
 pub struct StationAssets {
+    /// When the player is docked this ship is the ship the player sits in.
+    /// It will be used to undock and when interacting with stuff like repair.
+    /// The player can switch the ship and use one of the others in the station instead.
+    ///
+    /// When the player is docked None should be assumed as the default ship
+    /// This can happen when the player exploded.
+    /// When the player is not docked None is just "no one is docked anyway".
+    ///
+    /// This could have been realized as a pointer into the ships Vector but the are more things that can go wrong that way.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub current_ship: Option<Ship>,
+
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub ships: Vec<Ship>,
 
